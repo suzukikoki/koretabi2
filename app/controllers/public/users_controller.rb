35054e7ba9bd4277bf4a26db42b7576cb53b321c
suzukikoki <114800671+suzukikoki@users.noreply.github.com>
2022-12-13 13:33:12 +0000

    [add]12/13

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,49 @@
+class Public::UsersController < ApplicationController
+    #before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @user = current_user
+    @post_comments = @user.post_comments
+  end
+
+  def show
+    #@user = User.find(params[:id])
+    @user = current_user
+    @post_comments = @user.post_comments
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to current_user
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  def withdrawal
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  def unsubscribe
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
 
+  # 退会確認画面
+  get "/users/:id/unsubscribe" => "public/users#unsubscribe", as: "unsubscribe"
+  # 論理削除用のルーティング
+  patch "/users/:id/withdrawal" => "public/users#withdrawal", as: "withdrawal"
 
   #resource :favorites, only: [:create, :destroy]   
   #resources :post_comments, only: [:create, :destroy]
@@ -22,7 +26,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   scope module: :public do
    # resources :posts, only: [:index, :new, :create, :show, :update] 
-   
+    resources :users, only: [:index, :edit, :show, :update]
     resources :posts do
       resource :favorites, only: [:create, :destroy]
       resources :post_comments, only: [:create, :destroy]
